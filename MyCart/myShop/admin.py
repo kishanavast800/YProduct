9ab567f1f93dedc985dfc82fc098ce1b160bb928
rishipandey7 <46850937+rishipandey7@users.noreply.github.com>
2020-06-25 14:28:35 +0530

    Add files via upload

--- MyCart/myShop/admin.py
+++ MyCart/myShop/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product
+from .models import Product,Officestationary,Desktopstationary,Artiststationary,Writtingstationary,Paper_and_pad,Filling_and_storage_stationary,Electronicstationary,Printing_material
 
-admin.site.register(Product)
+admin.site.register(Product),
+admin.site.register(Officestationary),
+admin.site.register(Desktopstationary),
+admin.site.register(Artiststationary),
+admin.site.register(Writtingstationary),
+admin.site.register(Paper_and_pad),
+admin.site.register(Filling_and_storage_stationary),
+admin.site.register(Electronicstationary),
+admin.site.register(Printing_material)
+
--- MyCart/myShop/urls.py
+++ MyCart/myShop/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('tracker/',views.tracker,name='trackerStatus'),
     path('search/',views.search,name='Search'),
     path('viewproduct/',views.viewproduct,name='ViewProduct'),
+    path('Desktopstationary/',views.Desktop_stationary,name='Desktop_stationary'),
+    path('Artiststationary/',views.Artist_stationary,name='Artist_stationary'),
+    path('Writtingstationary/',views.Writting_stationary,name='Writting_stationary'),
+    path('Paper_and_pad/',views.Paperandpad,name='Paperandpad'),
+    path('Filling_and_storage_stationary/',views.Fillingandstoragestationary,name='Fillingandstoragestationary'),
+    path('Electronicstationary/',views.Electronic_stationary,name='Electronic_stationary'),
+    path('Printing_material/',views.Printingmaterial,name='Printingmaterial'),
     path('checkout/',views.checkout,name='checkout'),
 
 ]
