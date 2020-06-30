9b769df084474dd7bcf334ae6eb59ed1c532887d
kishan <kishansrivastava20@gmail.com>
2020-06-30 17:58:53 +0530

    fifth

--- MyCart/myShop/models.py
+++ MyCart/myShop/models.py
@@ -5,13 +5,18 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=100,default="")
-    pub_date = models.DateField()
-    image = models.ImageField(upload_to="myShop/images", default="")
-    def __str__(self):
-        return self.product_name
+    name = models.CharField(max_length=256, default="")
+    short_description = models.CharField(max_length=100, default="")
+    description = models.CharField(max_length=5000, default="")
+    category = models.CharField(max_length=100, default="")
+    secondary_category = models.CharField(max_length=500, default="")
+    image1 = models.ImageField(upload_to="myShop/images", default="")
+    image2 = models.ImageField(upload_to="myShop/images", default="")
+    image3 = models.ImageField(upload_to="myShop/images", default="")
+    price = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -19,108 +24,21 @@ class Contact(models.Model):
     phone = models.CharField(max_length=200, default="")
     desc = models.CharField(max_length=50000,default="")
 
-class Officestationary(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Desktopstationary(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Artiststationary(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Writtingstationary(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Paper_and_pad(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Filling_and_storage_stationary(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Electronicstationary(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-class Printing_material(models.Model):
-    products_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256,default="")
-    short_description = models.CharField(max_length=100, default="")
-    description = models.CharField(max_length=256,default="")
-    category = models.CharField(max_length=100,default="")
-    image1 = models.ImageField(upload_to="myShop/images", default="")
-    image2 = models.ImageField(upload_to="myShop/images", default="")
-    image3 = models.ImageField(upload_to="myShop/images", default="")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    def __str__(self):
-        return self.name
-
-
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key = True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=90)
+    amount = models.IntegerField(default=0)
+    email = models.CharField(max_length=120)
+    address = models.CharField(max_length=50000)
+    city = models.CharField(max_length=120)
+    state = models.CharField(max_length=120)
+    zip_code = models.CharField(max_length=120)
+    phone = models.CharField(max_length=120 , default="")
+class Request(models.Model):
+    msg_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, default="")
+    phone = models.CharField(max_length=200, default="")
+    desc = models.CharField(max_length=50000, default="")
+    image = models.ImageField(upload_to="myShop/images/", default="")
--- MyCart/myShop/views.py
+++ MyCart/myShop/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from .models import Product,Contact
-from .models import Product,Officestationary,Desktopstationary,Artiststationary,Writtingstationary,Paper_and_pad,Filling_and_storage_stationary,Electronicstationary,Printing_material
-
-
+import json
+from .models import Product, Orders, Contact, Request
 from math import ceil
 
 # Create your views here.
 def index(request):
     allprods = []
+
     catprods = Product.objects.values('category', 'product_id')
     print(catprods)
     cats = {item['category'] for item in catprods}
+    mats_pro = {item['category'] for item in catprods}
     list_old = list(cats)
-    list_new = sorted(list_old);
-
+    list_new = sorted(list_old)
+    lis_old = list(cats)
+    lis_new = sorted(list_old)
+    print(list_new)
     for cat in list_new:
         prod = Product.objects.filter(category=cat)
+        print(prod)
         n = len(prod)
         nSlides = n // 4 + ceil((n / 4) - (n // 4))
         allprods.append([prod, range(1, nSlides), nSlides])
 
     param = {'allprod': allprods}
+    print(param)
     return render(request,'myShop/Home.html',param)
 
 def about(request):
@@ -44,164 +47,46 @@ def tracker(request):
     return HttpResponse("tracker")
 def search(request):
     return HttpResponse("Search")
-def viewproduct(request):
-
-    return HttpResponse("viewproduct")
-def showCart(request):
-    return render(request,"myShop/cart.html")
-
-    allprods = []
-    catprods = Officestationary.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod = Officestationary.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/office.html',context)
-
-def Desktop_stationary(request):
-    allprods = []
-    catprods = Desktopstationary.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod = Desktopstationary.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Desktopstationary.html',context)
-
-def Artist_stationary(request):
-    allprods = []
-    catprods = Artiststationary.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod = Artiststationary.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Artiststationary.html',context)
-
-
-def Writting_stationary(request):
-    allprods = []
-    catprods = Writtingstationary.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod = Writtingstationary.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Writtingstationary.html',context)
-
-
-
-def Paperandpad(request):
-    allprods = []
-    catprods =  Paper_and_pad.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod =  Paper_and_pad.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Paper_and_pad.html',context)
-
-
-def Fillingandstoragestationary(request):
-    allprods = []
-    catprods =  Filling_and_storage_stationary.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod =  Filling_and_storage_stationary.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Filling_and_storage_stationary.html',context)    
-
-
-
-
-def Electronic_stationary(request):
-    allprods = []
-    catprods =  Electronicstationary.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod =  Electronicstationary.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Electronicstationary.html',context)    
-
-
-
-
-def Printingmaterial(request):
-    allprods = []
-    catprods =  Printing_material.objects.values('category', 'products_id')
-    print(catprods)
-    cats = {item['category'] for item in catprods}
-    list_old = list(cats)
-    list_new = sorted(list_old);
-    print(list_new)
-    for cat in list_new:
-        prod = Printing_material.objects.filter(category=cat)
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        allprods.append([prod, range(1, nSlides), nSlides])
-
-    context = {'allprod': allprods}
-    return render(request,'myShop/Printing_material.html',context)    
-
-
+def viewproduct(request,product_id):
+    print(product_id)
+    product = Product.objects.filter(product_id=product_id)[0]
+    print(product)
+    return render(request, 'myShop/productView.html', {'prod': product})
 
 
 
 def checkout(request):
-    return render(request,"myShop/checkout.html")
+    if request.method == "POST":
+        items_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name', '')
+        amount = request.POST.get('amount', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        phone = request.POST.get('phone', '')
+        zip_code = request.POST.get('zip_code', '')
+        order = Orders(items_json=items_json,name=name,email=email,amount=amount,address=address,city=city,state=state,phone=phone,zip_code=zip_code)
+        order.save()
+        thank = True;
+        id = order.order_id;
+        return render(request, 'myShop/checkout.html', {'thank': thank, 'id': id})
+    return render(request, 'myShop/checkout.html')
+
+
 def term(request):
     return render(request,"myShop/term.html")
+# this is for storing product request
 def villageContact(request):
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        image = request.POST.get('image','')
+        request_post = Request(name = name, email = email, phone = phone, desc = desc, image = image)
+        print(request_post)
+        request_post.save()
+        thanks = True
+        return render(request,'myShop/VillagePro.html',{'thanks':thanks})
     return render(request,"myShop/VillagePro.html")
